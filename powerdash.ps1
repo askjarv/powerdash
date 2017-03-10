6edb71f29bb265194f9d31c4aa84fc7501c699d5
Chris <askjarv@users.noreply.github.com>
2017-03-10 08:35:11 +0300

    powerdash.ps1
    Primary powershell script

---
+++ powerdash.ps1
@@ -0,0 +1,21 @@
+#requires -version 2
+<#
+.SYNOPSIS
+  <This script is used to generate the visualisation of your data>
+.DESCRIPTION
+  <Brief description of script>
+.PARAMETER <TBC>
+    <TBC>
+.INPUTS
+  <Your data lists>
+.OUTPUTS
+  <Chri>
+.NOTES
+  Version:        1.0
+  Author:         Christopher "AskJarv" Hudson
+  Creation Date:  10 March 2017
+  Purpose/Change: Initial script development
+  
+.EXAMPLE
+  <Example goes here. Repeat this attribute for more than one example>
+#>
